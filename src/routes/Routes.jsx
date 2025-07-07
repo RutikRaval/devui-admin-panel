8434e7dddfe2d451aeb33a0086ce7ec0e48008e3
rutik <rutikrawal.jainam@gmail.com>
2025-07-07 17:49:29 +0530

    add component api integration

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,9 +10,9 @@ import AddLanguage from "../pages/Language/AddLanguage";
 import ShowCategory from "../pages/Category/ShowCategory";
 import ShowLanguage from "../pages/Language/ShowLanguage";
 import App from "../App";
-import AddComponent from "../components/addComponent/AddComponent";
 // import AddComponent from "../pages/Component/AddComponent";
 import ShowComponent from "../pages/Component/ShowComponent";
+import AddComponent from "../pages/Component/AddComponent";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
--- src/services/componentApi.js
+++
@@ -1,5 +0,0 @@
-import axiosInstance from "./axiosInstance"
-
-export const getAllLanguageAPi=() => {
-    return axiosInstance.get("language/getalllanguage")
-}
